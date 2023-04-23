ccd41a15aaa3e0f58e0e64922f14d5024732f6cd
miku1001 <pauloted.22@gmail.com>
2023-04-23 15:43:54 +0800

    Pseudocodes

---
+++ odd_even.py
@@ -0,0 +1,13 @@
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers.
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers
+# extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from 
+# the numbers.txt.
+
+# open integers.txt(read), odd(append), even(append)
+# read integers.txt line by line
+# remove newline character and convert strings to integers
+# if even,
+# write to even.txt
+# if odd,
+# write to odd.txt
+# start
